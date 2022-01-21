553e4fc3625965f8be719a7accb8582e1f00cf3c
anton <voevodin_anton_2000@mail.ru>
2022-01-21 22:36:02 +0400

    wget - removing unnecessary method

--- tasks/wget/pkg/siteParser.go
+++ tasks/wget/pkg/siteParser.go
@@ -9,22 +9,6 @@ import (
 	"strings"
 )
 
-func getResponse(url string) (*http.Response, error) {
-	client := http.Client{
-		CheckRedirect: func(r *http.Request, via []*http.Request) error {
-			r.URL.Opaque = r.URL.Path
-			return nil
-		},
-	}
-
-	res, err := client.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	return res, nil
-}
-
 func parseSite(site string, filename string) {
 	siteHTML, _ := goquery.ParseUrl(site)
 	writeFile(filename, strings.NewReader(siteHTML.Html()))
